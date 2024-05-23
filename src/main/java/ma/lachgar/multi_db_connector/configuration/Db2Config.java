60ad215579e279c835c5b7c76ce9be3a37da5471
yousseflach <lachgar.youssef6174@gmail.com>
2024-05-23 15:39:20 +0100

    configured Db2Config

--- src/main/java/ma/lachgar/multi_db_connector/configuration/Db2Config.java
+++ src/main/java/ma/lachgar/multi_db_connector/configuration/Db2Config.java
@@ -1,4 +1,50 @@
 package ma.lachgar.multi_db_connector.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "db2EntityManagerFactory",
+        transactionManagerRef = "db2TransactionManager",
+//        ma/lachgar/multi_db_connector/db2
+        basePackages = { "ma.lachgar.multi_db_connector.db2.repository" }
+)
 public class Db2Config {
+    @Bean(name="db2DataSource")
+    @ConfigurationProperties(prefix="spring.db2.datasource")
+    public DataSource db2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "db2EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean db2EntityManagerFactory(EntityManagerFactoryBuilder builder,
+                                                                          @Qualifier("db2DataSource") DataSource db2DataSource) {
+        return builder
+                .dataSource(db2DataSource)
+                .packages("ma.lachgar.multi_db_connector.db2.entity")
+                .build();
+    }
+
+    @Bean(name = "db2TransactionManager")
+    public PlatformTransactionManager db2TransactionManager(
+            @Qualifier("db2EntityManagerFactory") EntityManagerFactory db2EntityManagerFactory) {
+        return new JpaTransactionManager(db2EntityManagerFactory);
+    }
+
 }
+
