dfaabe7274c3a954315dd1402b5f891647e70920
yousseflach <lachgar.youssef6174@gmail.com>
2024-05-23 14:43:51 +0100

    intialized project with needed dependencies

---
+++ src/main/java/ma/lachgar/multi_db_connector/MultiDbConnectorApplication.java
@@ -0,0 +1,13 @@
+package ma.lachgar.multi_db_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDbConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDbConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/lachgar/multi_db_connector/MultiDbConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.lachgar.multi_db_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDbConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
